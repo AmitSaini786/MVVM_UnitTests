5098e8da1d0aaa76b7003507109f570cd5441407
Amit Saini <amitsaini703@gmail.com>
2023-02-23 14:48:07 +0100

    Initial Commit

---
+++ NGBL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NGBL
+//
+//  Created by Amit on 23/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
